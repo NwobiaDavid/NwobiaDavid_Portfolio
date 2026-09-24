11835619e57c34ac3e95c5c93404bccc3450e6cd
Dave <dnwobia@gmail.com>
2026-09-24 13:39:20 +0100

    remove unused blobity hook and legacy loader styles

--- src/lib/theme.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Checks if the user's system has a dark color scheme.
- * @returns {boolean} True if the system has a dark color scheme, false otherwise.
- */
-export const isDarkSystem = window.matchMedia(
-    "(prefers-color-scheme: dark)"
-  ).matches;
